6223125c931454aff69b6832ad5529ba080508a9
blinkythefish <pottle.jamin@gmail.com>
2022-10-06 18:37:38 -0600

    fixed dealer so it does not display next card

--- dealer.py
+++ dealer.py
@@ -7,7 +7,6 @@ class Dealer:
 #This method gets a random number between 1 and 13, this method should be used when starting the game to get a number card. This represents the current card on the board
     def random_card(self):
         self.card=randint(1,13)
-        # print(self.card)
         return self.card
     
 #This method gets a random number between 1 and 13, this method represents the next card threw on the board, the number should be a higher or lower number than the random_card method
@@ -15,13 +14,8 @@ class Dealer:
         self.new_card=randint(1,13)
         while self.card == self.new_card:
             self.new_card=randint(1,13)
-
-        print(self.new_card)
         return self.new_card
 
-#deal=Dealer()
-#deal.random_card()
-#deal.next_card()
 
 
         
